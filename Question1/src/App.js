fcecc38bb54701fa2bae77ff7b65f16e7416013c
kspl456 <146167786+kspl456@users.noreply.github.com>
2025-08-19 11:35:07 +0530

    updated-header

--- Question1/src/App.js
+++ Question1/src/App.js
@@ -5,20 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div className='brand'>
+        <span className='logo'></span>
+        <span className='title'>Url Shortner</span>
+       </div>
+       <nav className='nav'>
+        <a href='#/shortner'>Shortner</a>
+        <a href='#/stats'>Statistics</a>
+       </nav>
       </header>
     </div>
+
+    
   );
 }
 
